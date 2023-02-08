a52d05f016b238fb93c9ddf319fb8169dc4dfb43
Keith Naber <knaber4124@gmail.com>
2023-02-08 14:23:44 -0500

    added bootstrap w/ script updates. ngif and ngfor

--- src/app/components/users-list/users-list.component.ts
+++ src/app/components/users-list/users-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { User } from 'src/app/models/User';
 
 @Component({
   selector: 'app-users-list',
@@ -6,9 +7,66 @@ import { Component } from '@angular/core';
   styleUrls: ['./users-list.component.css']
 })
 export class UsersListComponent {
+  users!: User[];
+  showExtended: boolean = true;
+  loaded: boolean = false;
+
+  constructor() { }
 
   ngOnInit() {
-    console.log('BOO')
+    this.users =
+      [
+        {
+          firstName: 'John',
+          lastName: 'Doe',
+          age: 30,
+          address: {
+            street: '1 Main St',
+            city: 'Boston',
+            state: 'MA'
+          }
+        },
+        {
+          firstName: 'Kevin',
+          lastName: 'Smith',
+          age: 25,
+          address: {
+            street: '25 Boylston St',
+            city: 'Boston',
+            state: 'MA'
+          }
+        },
+        {
+          firstName: 'Cathy',
+          lastName: 'Richards',
+          age: 28,
+          address: {
+            street: '100 Commonwealth Ave',
+            city: 'Boston',
+            state: 'MA'
+          }
+        }
+      ];
+    this.loaded = true
+
+
+
+    // this.showExtended = false;
+
+    this.addUser({
+      firstName: 'Sally',
+      lastName: 'Fields',
+      // age: 60
+      // address: {
+      //   street: '1200 Elm St',
+      //   city: 'Los Angeles',
+      //   state: 'CA'
+      // }
+    })
+  }
+
+  addUser(user: User) {
+    this.users.push(user);
   }
 
 
